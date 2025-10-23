9db371ba573fe489f1a1b643f0c769fbfee578d9
pedro <pedro.collares05@gmai.com>
2025-10-23 19:23:40 -0300

    Fix formatting in App.tsx by adding missing semicolons for consistency

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -1,13 +1,11 @@
-import { Header } from "./components/layout/Header"
-
+import { Header } from "./components/layout/Header";
 
 function App() {
-
   return (
     <>
       <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
