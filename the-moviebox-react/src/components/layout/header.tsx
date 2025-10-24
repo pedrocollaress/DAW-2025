6a2278d69ad5f86ca5c33f59da06047516e5f5f4
pedro <pedro.collares05@gmai.com>
2025-10-23 21:50:49 -0300

    feat: integrate Tailwind CSS and refactor layout components
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Refactored Header and Footer components to use Tailwind CSS for styling.
    - Removed old Header and Footer component files and created new ones with updated styles.
    - Updated index.css to import Tailwind CSS and removed unnecessary styles.
    - Created a new FooterLink component for consistent link styling in the footer.

---
+++ the-moviebox-react/src/components/layout/header.tsx
@@ -0,0 +1,25 @@
+export function Header() {
+  return (
+    <header className="px-4 py-8">
+      <div className="mx-auto max-w-6xl flex flex-row items-center justify-between">
+        <h1 className="text-2xl font-bold tracking-wide text-white">
+          <span className="font-normal">THE</span>MOVIEBOX
+        </h1>
+        <div className="flex gap-4">
+          <a
+            className="text-white text-sm py-2 px-4 border border-[#333] rounded-sm transition hover:bg-[#333]"
+            href="#"
+          >
+            Entrar
+          </a>
+          <a
+            className="text-white text-sm py-2 px-4 bg-[#e50914] border border-[#e50914] rounded-sm transition hover:bg-[#b8070f]"
+            href="#"
+          >
+            Registrar
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+}
