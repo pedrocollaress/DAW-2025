0cd61a37bca47c2829521be711250a5d34779a2d
pedro <pedro.collares05@gmai.com>
2025-11-06 21:53:13 -0300

    feat: implement loading state in MovieContent and refactor MovieService to fetch data from API

--- the-moviebox-react/src/cases/movies/components/movie-content.tsx
+++ the-moviebox-react/src/cases/movies/components/movie-content.tsx
@@ -4,14 +4,24 @@ import { MovieCard } from "./movie-card";
 
 export function MovieContent() {
   const [movies, setMovies] = useState<MovieDTO[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    MovieService.list().then((result) => {
-      setMovies(result);
-    });
+    setIsLoading(true);
+    MovieService.list()
+      .then((result) => {
+        setMovies(result);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
-  return (
+  return isLoading ? (
+    <div className="bg-[#1c1c1c] flex justify-center items-center h-40">
+      <p className="text-2xl text-center text-white">Carregando...</p>
+    </div>
+  ) : (
     <section className="bg-[#1c1c1c] p-8 grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-8 max-w-6xl mx-auto">
       {movies.map((movies, index) => (
         <MovieCard key={index} movie={movies} />
